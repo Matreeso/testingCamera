8f806cc7d5652b166bde141d93750d6c299de751
Saad Bajwa <saadbajwa818@gmail.com>
2025-04-25 22:07:49 -0700

    Initial Commit

---
+++ testingCamera/testingCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingCameraApp.swift
+//  testingCamera
+//
+//  Created by Saad Bajwa on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct testingCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
